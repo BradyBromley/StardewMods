d7f012d4318d167615cd43bda5a24f46a4bc5b8b
BradyBromley <bradybromley@live.ca>
2024-05-28 14:41:51 -0700

    closes #2: New config options added for icon size

---
+++ FarmIconOnLoadScreen/ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FarmIconOnLoadScreen
+{
+    internal class ModConfig
+    {
+        public bool SmallIcons { get; set; }
+
+        public ModConfig()
+        {
+            this.SmallIcons = false;
+        }
+
+    }
+}
--- FarmIconOnLoadScreen/ModEntry.cs
+++ FarmIconOnLoadScreen/ModEntry.cs
@@ -11,18 +11,26 @@ namespace FarmIconOnLoadScreen
 {
     public class ModEntry : Mod
     {
+        private ModConfig Config;
+
         public override void Entry(IModHelper helper)
         {
-            helper.Events.GameLoop.SaveCreating += this.SaveFarmType;
-            helper.Events.GameLoop.Saving += this.SaveFarmType;
+            // Read or Create config file
+            Config = this.Helper.ReadConfig<ModConfig>();
 
-            FarmIconPatch.Initialize(this);
-
+            // Harmony patching
             var harmony = new Harmony(this.ModManifest.UniqueID);
             harmony.Patch(
                original: AccessTools.Method(typeof(StardewValley.Menus.LoadGameMenu.SaveFileSlot), nameof(StardewValley.Menus.LoadGameMenu.SaveFileSlot.Draw)),
                postfix: new HarmonyMethod(typeof(FarmIconPatch), nameof(FarmIconPatch.DrawFarmIconPostfix))
             );
+
+            // Setup Events
+            helper.Events.GameLoop.SaveCreating += SaveFarmType;
+            helper.Events.GameLoop.Saving += SaveFarmType;
+            helper.Events.GameLoop.GameLaunched += SetupConfig;
+
+            FarmIconPatch.Initialize(Monitor, Config);
         }
 
         // Save the farm type in farmer modData after creating a farmer so that it can be easily accessed later
@@ -46,6 +54,36 @@ namespace FarmIconOnLoadScreen
             {
                 Monitor.Log($"Failed in {nameof(SaveFarmType)}:\n{ex}", LogLevel.Error);
             }
+        }
+
+        // Set up the GenericModConfigMenu
+        private void SetupConfig(object? sender, EventArgs e)
+        {
+            // Get GenericModConfigMenu API
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+            {
+                return;
+            }
+
+            // Register Mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () =>
+                {
+                    Config = new ModConfig();
+                    Helper.WriteConfig(Config);
+                },
+                save: () => Helper.WriteConfig(Config)
+            );
+
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                getValue: () => Config.SmallIcons,
+                setValue: value => Config.SmallIcons = value,
+                name: () => "Small Icons",
+                tooltip: () => "Smaller farm icons on save files"
+            );
         }
     }
 
